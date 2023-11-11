6218ce95beec1ed0d1e35feda56081cc6fafc002
Kushal Gupta <98078018+guptakushal03@users.noreply.github.com>
2023-11-11 15:55:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Simulate loading delay (you can remove this in a real scenario)
     setTimeout(function () {
         document.querySelector('.loader-container').style.display = 'none';
         document.querySelector('.content').style.display = 'block';
-    }, 2000);
+    }, 2000); // Adjust the time as needed
 });
 
 var navLinks = document.getElementById("navLinks");
@@ -11,4 +12,12 @@ function showMenu(){
 }
 function hideMenu(){
     navLinks.style.right = "-200px";
+}
+
+function showAlert() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+    alert("Too lazy to handle forms, but u sure can contact me using my social links!");
 }
